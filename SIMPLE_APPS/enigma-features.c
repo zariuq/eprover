29f9e4a41a8586b2359097fc55ceb4fbddbef38f
Jan Jakubuv <jakubuv@gmail.com>
2019-07-26 02:01:30 +0200

    Support hashing-only version from now on

--- SIMPLE_APPS/enigma-features.c
+++ SIMPLE_APPS/enigma-features.c
@@ -71,12 +71,12 @@ OptCell opts[] =
       "Enigma features to be generate. The value is a string of characters "
       "determining the features to be used. Valid characters are 'V' "
       "(vertical features), 'H' (horizontal), 'S' (symbols), 'L' "
-      "(length stats), 'C' (conjecture), 'h' (hashing)."},
+      "(length stats), 'C' (conjecture)."},
    {OPT_FEATURE_HASHING,
       '\0', "feature-hashing",
       ReqArg, NULL,
-      "Turn on Enigma features hashing.  The argument specifies the numeric "
-      "hash base (for example, 32768, that is, 2^15)."},
+      "Enigma features hashing is always on.  The argument specifies the numeric "
+      "hash base.  The default is 32768, that is, 2^15."},
    {OPT_FREE_NUMBERS,
     '\0', "free-numbers",
      NoArg, NULL,
@@ -92,7 +92,7 @@ OptCell opts[] =
 char *outname = NULL;
 FunctionProperties free_symb_prop = FPIgnoreProps;
 EnigmaFeatures Enigma = EFAll;
-unsigned long FeatureHashing = 0L;
+unsigned long FeatureHashing = 32768L;
 
 /*---------------------------------------------------------------------*/
 /*                      Forward Declarations                           */
@@ -105,209 +105,6 @@ void print_help(FILE* out);
 /*                         Internal Functions                          */
 /*---------------------------------------------------------------------*/
 
-
-static char* top_symbol_string(Term_p term, Sig_p sig)
-{
-   if (TermIsVar(term)) 
-   {
-      return ENIGMA_VAR;
-   }
-   else 
-   {
-      char* name = SigFindName(sig, term->f_code);
-      if (name[0] == 'e') // be fast
-      {
-         if ((strncmp(name, "esk", 3) == 0) || (strncmp(name, "epred", 5) == 0))
-         {
-            return ENIGMA_SKO;
-         }
-      }
-      return name;
-   }
-}
-
-static void term_features_string(DStr_p str, Term_p term, Sig_p sig, char* sym1, char* sym2)
-{
-   assert(Enigma & EFVertical);
-
-   char* sym3 = top_symbol_string(term, sig);
-
-   // vertical features
-   if (Enigma & EFVertical)
-   {
-      DStrAppendStr(str, sym1); DStrAppendChar(str, ':');
-      DStrAppendStr(str, sym2); DStrAppendChar(str, ':');
-      DStrAppendStr(str, sym3); DStrAppendChar(str, ' ');
-   }
-   
-   if (TermIsVar(term)||(TermIsConst(term))) 
-   {
-      return;
-   }
-   for (int i=0; i<term->arity; i++)
-   {
-      term_features_string(str, term->args[i], sig, sym2, sym3);
-   }
-
-   // horizontal features (functions only)
-   if (Enigma & EFHorizontal)
-   {
-      DStr_p hstr = FeaturesGetTermHorizontal(sym3, term, sig);
-      DStrAppendDStr(str, hstr);
-      DStrAppendChar(str, ' ');
-      DStrFree(hstr);
-   }
-}
-
-static void clause_features_string(DStr_p str, Clause_p clause, Sig_p sig, long* vec)
-{
-   for(Eqn_p lit = clause->literals; lit; lit = lit->next)
-   {
-      char* sym1 = EqnIsPositive(lit)?ENIGMA_POS:ENIGMA_NEG;
-      if (lit->rterm->f_code == SIG_TRUE_CODE)
-      {
-         char* sym2 = SigFindName(sig, lit->lterm->f_code);
-
-         // traverse verticals/horizontals 
-         for (int i=0; i<lit->lterm->arity; i++) // here we ignore prop. constants
-         {
-            term_features_string(str, lit->lterm->args[i], sig, sym1, sym2);
-         }
-
-         // top-level horizontal
-         if ((Enigma & EFHorizontal) && (lit->lterm->arity > 0))
-         {
-            DStr_p hstr = FeaturesGetTermHorizontal(sym2, lit->lterm, sig);
-            DStrAppendDStr(str, hstr);
-            DStrAppendChar(str, ' ');
-            DStrFree(hstr);
-         }
-      }
-      else
-      {
-         char* sym2 = ENIGMA_EQ;
-         
-         // traverse verticals/horizontals 
-         term_features_string(str, lit->lterm, sig, sym1, sym2);
-         term_features_string(str, lit->rterm, sig, sym1, sym2);
-
-         // top-level horizontal
-         if (Enigma & EFHorizontal)
-         {
-            DStr_p hstr = FeaturesGetEqHorizontal(lit->lterm, lit->rterm, sig);
-            DStrAppendDStr(str, hstr);
-            DStrAppendChar(str, ' ');
-            DStrFree(hstr);
-         }
-      }
-   }
-
-   if (vec)
-   {
-      PStack_p mod_stack = PStackAlloc();
-      ClauseAddSymbolFeatures(clause, mod_stack, vec);
-      PStackFree(mod_stack);
-   }
-}
-
-static void clause_static_features_string(DStr_p str, long* vec, Sig_p sig, long* vars)
-{
-   static char fstr[1024];
-
-   if (Enigma & EFLengths) 
-   {
-      snprintf(fstr, 1024, "!LEN/%ld ", vec[0]); DStrAppendStr(str, fstr);
-      snprintf(fstr, 1024, "!POS/%ld ", vec[1]); DStrAppendStr(str, fstr);
-      snprintf(fstr, 1024, "!NEG/%ld ", vec[2]); DStrAppendStr(str, fstr);
-   }
-
-   // if (variable features) ...
-   if (vars && (Enigma & EFVariables))
-   {
-      snprintf(fstr, 1024, "#!COUNT/%ld ", vars[0]); DStrAppendStr(str, fstr);
-      snprintf(fstr, 1024, "#!OCCUR/%ld ", vars[1]); DStrAppendStr(str, fstr);
-      snprintf(fstr, 1024, "#!UNIQ/%ld ",  vars[2]); DStrAppendStr(str, fstr);
-      snprintf(fstr, 1024, "#!SHARE/%ld ", vars[3]); DStrAppendStr(str, fstr);
-      snprintf(fstr, 1024, "%%!MAX1/%ld ", vars[4]); DStrAppendStr(str, fstr);
-      snprintf(fstr, 1024, "%%!MAX2/%ld ", vars[5]); DStrAppendStr(str, fstr);
-      snprintf(fstr, 1024, "%%!MAX3/%ld ", vars[6]); DStrAppendStr(str, fstr);
-   }
-
-   if (!(Enigma & EFSymbols)) 
-   {
-      return;
-   }
-   
-   for (long f=sig->internal_symbols+1; f<=sig->f_count; f++)
-   {
-      char* fname = SigFindName(sig, f);
-      if ((strlen(fname)>3) && ((strncmp(fname, "esk", 3) == 0) || (strncmp(fname, "epred", 5) == 0)))
-      {
-         continue;
-      }
-      if (vec[3+4*f+0] > 0) 
-      {
-         snprintf(fstr, 1024, "#+%s/%ld ", fname, vec[3+4*f+0]); DStrAppendStr(str, fstr);
-         snprintf(fstr, 1024, "%%+%s/%ld ", fname, vec[3+4*f+1]+1); DStrAppendStr(str, fstr);
-      }
-      if (vec[3+4*f+2] > 0) 
-      {
-         snprintf(fstr, 1024, "#-%s/%ld ", fname, vec[3+4*f+2]); DStrAppendStr(str, fstr);
-         snprintf(fstr, 1024, "%%-%s/%ld ", fname, vec[3+4*f+3]+1); DStrAppendStr(str, fstr);
-      }
-   }
-}
-
-static DStr_p get_conjecture_features_string(char* filename, TB_p bank)
-{
-   static long* vec = NULL;
-   static size_t size = 0;
-   NumTree_p stat = NULL;
-   long vars[10] = { 0 };
-   int offset = 0;
-
-   if (!vec)
-   {
-      size = (3+4*64)*sizeof(long); // start with memory for 64 symbols
-      vec = RegMemAlloc(size);
-   }
-   vec = RegMemProvide(vec, &size, (3+4*(bank->sig->f_count+1))*sizeof(long));
-   for (int i=0; i<3+4*(bank->sig->f_count+1); i++) { vec[i] = 0L; }
-
-   DStr_p str = DStrAlloc();
-   Scanner_p in = CreateScanner(StreamTypeFile, filename, true, NULL);
-   ScannerSetFormat(in, TSTPFormat);
-   while (TestInpId(in, "cnf"))
-   {
-      Clause_p clause = ClauseParse(in, bank);
-      vec = RegMemProvide(vec, &size, (3+4*(bank->sig->f_count+1))*sizeof(long));
-      if (ClauseQueryTPTPType(clause) == CPTypeNegConjecture) 
-      {
-         vec[0] += (long)ClauseWeight(clause,1,1,1,1,1,false);
-         vec[1] += clause->pos_lit_no;
-         vec[2] += clause->neg_lit_no;
-         clause_features_string(str, clause, bank->sig, &vec[3]);
-         if (Enigma & EFVariables)
-         {
-            int distinct = 0;
-            FeaturesClauseVariablesExtend(&stat, clause, &distinct, offset);
-            offset += (2 * distinct);
-         }
-      }
-      ClauseFree(clause);
-   }
-   CheckInpTok(in, NoToken);
-   DestroyScanner(in);
-
-   if (Enigma & EFVariables)
-   {
-      FeaturesClauseVariablesStat(&stat, vars);
-      NumTreeFree(stat);
-   }
-   clause_static_features_string(str, vec, bank->sig, vars);
-   DStrDeleteLastChar(str);
-   return str;
-}
 
 static NumTree_p get_conjecture_features(char* filename, TB_p bank, Enigmap_p enigmap)
 {
@@ -330,47 +127,6 @@ static NumTree_p get_conjecture_features(char* filename, TB_p bank, Enigmap_p en
    DestroyScanner(in);
 
    return features;
-}
-
-static void dump_features_strings(FILE* out, char* filename, TB_p bank, char* prefix, char* conj)
-{
-   long vars[10];
-   DStr_p str = DStrAlloc();
-   Scanner_p in = CreateScanner(StreamTypeFile, filename, true, NULL);
-   ScannerSetFormat(in, TSTPFormat);
-
-   static long* vec = NULL;
-   static size_t size = 0;
-   if (!vec)
-   {
-      size = (3+4*64)*sizeof(long); // start with memory for 64 symbols
-      vec = RegMemAlloc(size);
-   }
-   
-   while (TestInpId(in, "cnf"))
-   {
-      Clause_p clause = ClauseParse(in, bank);
-      vec = RegMemProvide(vec, &size, (3+4*(bank->sig->f_count+1))*sizeof(long));
-      for (int i=0; i<3+4*(bank->sig->f_count+1); i++) { vec[i] = 0L; }
-      
-      vec[0] = (long)ClauseWeight(clause,1,1,1,1,1,false);
-      vec[1] = clause->pos_lit_no;
-      vec[2] = clause->neg_lit_no;
-      clause_features_string(str, clause, bank->sig, &vec[3]);
-      if (Enigma & EFVariables) 
-      {
-         FeaturesClauseVariables(clause, vars);
-      }
-      clause_static_features_string(str, vec, bank->sig, vars);
-
-      DStrDeleteLastChar(str);
-      fprintf(out, "%s|%s|%s\n", prefix, DStrView(str), conj);
-      DStrReset(str);
-      ClauseFree(clause);
-   }
-   CheckInpTok(in, NoToken);
-   DestroyScanner(in);
-   DStrFree(str);
 }
 
 static void dump_features_hashes(FILE* out, char* filename, TB_p bank, char* prefix, NumTree_p conj_features, Enigmap_p enigmap)
@@ -420,51 +176,26 @@ int main(int argc, char* argv[])
    TB_p bank = state->terms;
   
    DStr_p dstr = NULL;
-   char* conj = "";
    NumTree_p conj_features = NULL;
+
    Enigmap_p enigmap = NULL;
-   if (FeatureHashing) 
-   {
-      enigmap = EnigmapAlloc();
-      enigmap->feature_count = FeatureHashing;
-      enigmap->version = Enigma;
-      enigmap->sig = bank->sig;
-   }
+   enigmap = EnigmapAlloc();
+   enigmap->feature_count = FeatureHashing;
+   enigmap->version = Enigma;
+   enigmap->sig = bank->sig;
+
    if ((Enigma & EFConjecture) && (args->argc == 3))
    {
-      if (FeatureHashing)
-      {
-         conj_features = get_conjecture_features(args->argv[2], bank, enigmap);
-      }
-      else
-      {
-         dstr = get_conjecture_features_string(args->argv[2], bank);
-         conj = DStrView(dstr); 
-      }
+      conj_features = get_conjecture_features(args->argv[2], bank, enigmap);
    }
    if (args->argc == 1)
    {
-      if (FeatureHashing)
-      {
-         dump_features_hashes(GlobalOut, args->argv[0], bank, "+?", conj_features, enigmap);
-      }
-      else
-      {
-         dump_features_strings(GlobalOut, args->argv[0], bank, "*", conj);
-      }
+      dump_features_hashes(GlobalOut, args->argv[0], bank, "+?", conj_features, enigmap);
    }
    else
    {
-      if (FeatureHashing)
-      {
-         dump_features_hashes(GlobalOut, args->argv[0], bank, "+1", conj_features, enigmap);
-         dump_features_hashes(GlobalOut, args->argv[1], bank, "+0", conj_features, enigmap);
-      }
-      else
-      {
-         dump_features_strings(GlobalOut, args->argv[0], bank, "+", conj);
-         dump_features_strings(GlobalOut, args->argv[1], bank, "-", conj);
-      }
+      dump_features_hashes(GlobalOut, args->argv[0], bank, "+1", conj_features, enigmap);
+      dump_features_hashes(GlobalOut, args->argv[1], bank, "+0", conj_features, enigmap);
    }
 
    if (dstr) { DStrFree(dstr); }
